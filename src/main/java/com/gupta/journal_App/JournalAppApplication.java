d34a4a06960ace6eeaff05fece3afd90c4f232c3
Prateek Gupta <68748725+prateekgupta2411@users.noreply.github.com>
2025-02-12 22:55:29 +0530

    Initial commit

---
+++ src/main/java/com/gupta/journal_App/JournalAppApplication.java
@@ -0,0 +1,13 @@
+package com.gupta.journal_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalAppApplication.class, args);
+	}
+
+}
