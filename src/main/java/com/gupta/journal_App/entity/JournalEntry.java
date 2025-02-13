07987f489c30a4266221b2a386da0dbe04f3084d
Prateek Gupta <68748725+prateekgupta2411@users.noreply.github.com>
2025-02-13 16:41:17 +0530

    Second day

--- src/main/java/com/gupta/journal_App/entity/JournalEntry.java
+++ src/main/java/com/gupta/journal_App/entity/JournalEntry.java
@@ -1,16 +1,34 @@
 package com.gupta.journal_App.entity;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Document(collection = "journal_entries")
 public class JournalEntry {
-    private Long id;
+    @Id
+    private ObjectId id;
     private String title;
     private String content;
+    private LocalDateTime date;
     // Constructor, getters, and setters
 
-    public Long getId() {
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public ObjectId getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(ObjectId id) {
         this.id = id;
     }
 
